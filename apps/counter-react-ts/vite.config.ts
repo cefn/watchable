6c78272cf21538a37eef7abae23c9957960bef93
Cefn Hoile <github.com@cefn.com>
2023-04-17 07:58:30 +0100

    WIP preact experiment in react-ts

--- apps/counter-react-ts/vite.config.ts
+++ apps/counter-react-ts/vite.config.ts
@@ -2,13 +2,39 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
 
+// import { visualizer } from "rollup-plugin-visualizer";
+// import alias from "@rollup/plugin-alias";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: "terser",
+    rollupOptions: {
+      treeshake: true,
+      // plugins: [
+      //   alias({
+      //     entries: [
+      //       { find: "react", replacement: "preact/compat" },
+      //       { find: "react-dom/test-utils", replacement: "preact/test-utils" },
+      //       { find: "react-dom", replacement: "preact/compat" },
+      //       { find: "react/jsx-runtime", replacement: "preact/jsx-runtime" },
+      //     ],
+      //   }),
+      // ],
+    },
+  },
   plugins: [
     react(),
     legacy({
       targets: ["defaults"],
     }),
+    // visualizer({
+    //   template: "treemap", // or sunburst
+    //   open: true,
+    //   gzipSize: true,
+    //   brotliSize: true,
+    //   filename: "analice.html",
+    // }),
   ],
   clearScreen: false,
   server: {
