a9f61b3e285ca778cbea620f33a3a6f26b3b330a
Cefn Hoile <github.com@cefn.com>
2023-04-23 21:20:29 +0100

    Typedoc wrongly interprets tilde as strikethrough

--- packages/store-edit/src/edit.ts
+++ packages/store-edit/src/edit.ts
@@ -4,11 +4,11 @@ import { produce } from "immer";
 
 /** Accepts an {@link Editor} function which will be passed a `draft` of the current
  * state. The function can manipulate the draft state using normal javascript
- * assignments and operations as if it ***wasn't*** {@link @lauf/store~Immutable}. When it
- * returns, {@link @lauf/store~Store#write} will be called on your behalf with the equivalent
- * {@link @lauf/store~Immutable} result.
+ * assignments and operations as if it ***wasn't*** {@link @lauf/store.Immutable}. When it
+ * returns, {@link @lauf/store.Store#write} will be called on your behalf with the equivalent
+ * {@link @lauf/store.Immutable} result.
  * @param editor A function to draft the next state
- * @returns The resulting new {@link @lauf/store~Immutable} state aligned with your draft changes. */
+ * @returns The resulting new {@link @lauf/store.Immutable} state aligned with your draft changes. */
 export function edit<State extends RootState>(
   store: Store<State>,
   editor: Editor<State>
--- packages/store-edit/src/types.ts
+++ packages/store-edit/src/types.ts
@@ -7,11 +7,11 @@ import type { Draft } from "immer";
  * You can make changes to the mutable `draft` proxy within your editor callback
  * using any javascript syntax. When it returns,
  * {@link https://immerjs.github.io/immer/ | Immer} efficiently composes a new
- * {@link @lauf/store~Immutable} state to reflect your drafted changes, leaving the old state
- * intact. The new state is passed to {@link @lauf/store~Store#write}.
+ * {@link @lauf/store.Immutable} state to reflect your drafted changes, leaving the old state
+ * intact. The new state is passed to {@link @lauf/store.Store#write}.
  *
  * The editor is equivalent to Immer's producer except returning a value doesn't
- * replace the {@link @lauf/store~RootState}. To replace the state call {@link @lauf/store~Store#write} instead
+ * replace the {@link @lauf/store.RootState}. To replace the state call {@link @lauf/store.Store#write} instead
  * of using an editor. This eliminates Immer's runtime errors when you **draft**
  * changes as well as returning a value, (easily done by accident in simple
  * arrow functions).
@@ -19,7 +19,7 @@ import type { Draft } from "immer";
  * See {@link https://immerjs.github.io/immer/ | Immer docs} for more detail on
  * the conventions for Immer `producers`.
  *
- * @param draft A mutable proxy of a {@link @lauf/store~Store}'s existing `Immutable` state.
+ * @param draft A mutable proxy of a {@link @lauf/store.Store}'s existing `Immutable` state.
  * Manipulate this object to compose the next state.
  * */
 export type Editor<T> = (draft: Draft<T>) => void;
