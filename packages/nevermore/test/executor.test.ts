ac77553c71e06303dfa41a7d21c316b5261da7e4
Cefn Hoile <choile@roku.com>
2023-12-05 08:43:20 +0000

    Add executor tests

--- packages/nevermore/test/executor.test.ts
+++ packages/nevermore/test/executor.test.ts
@@ -136,155 +136,193 @@ describe("createExecutor ", () => {
     ]);
   });
 
-  test("executor with retry can try again and succeed", async () => {
-    // TEST SETUP
-    const FAILURES = 1;
-
+  describe("Retry", () => {
     const { createExecutor } = createExecutorStrategy({ retries: 2 });
 
-    const createFailingOperation = (failures: number) => {
-      let attempt = 0;
+    // factory for an operation which will fail a controllable number of times
+    const createFailingOperation = (failuresEmulated: number) => {
+      let failedAttempts = 0;
+
       return async (n: number) => {
-        attempt++;
-        if (attempt <= failures) {
+        if (failedAttempts < failuresEmulated) {
+          failedAttempts++;
           throw new Error("Job failed");
         }
         return n;
       };
     };
 
-    // CREATE OPERATIONS AND EXECUTORS
-    const operations = Array.from({ length: 4 }, () =>
-      createFailingOperation(FAILURES)
-    );
-    const executors = operations.map((operation) => createExecutor(operation));
+    test("executor with retry can try again and succeed", async () => {
+      // TEST SETUP
+      const FAILURES = 1;
 
-    // EXECUTE OPERATIONS AND EXECUTORS
-    const operationSettlements = await Promise.allSettled(
-      operations.map((operation, index) => operation(index))
-    );
-    const executorSettlements = await Promise.allSettled(
-      executors.map((executor, index) => executor(index))
-    );
+      // CREATE OPERATIONS AND EXECUTORS
+      const operations = [
+        createFailingOperation(FAILURES),
+        createFailingOperation(FAILURES),
+        createFailingOperation(FAILURES),
+        createFailingOperation(FAILURES),
+      ];
 
-    // CHECK RESULTS FROM OPERATIONS AND EXECUTORS
+      const executors = [
+        createExecutor(operations[0]),
+        createExecutor(operations[1]),
+        createExecutor(operations[2]),
+        createExecutor(operations[3]),
+      ];
 
-    // all operations will be run once
-    // and will fail
-    expect(
-      operationSettlements.every(
-        (settlement) => settlement.status === "rejected"
-      )
-    );
+      // EXECUTE OPERATIONS AND EXECUTORS
+      const operationSettlements = await Promise.allSettled(
+        operations.map((operation, index) => operation(index))
+      );
 
-    // all executors will be run once (but underlying operation run twice)
-    // and will therefore succeed
-    expect(
-      executorSettlements.every(
-        (settlement) => settlement.status === "fulfilled"
-      )
-    );
+      const executorSettlements = await Promise.allSettled(
+        executors.map((executor, index) => executor(index))
+      );
 
-    // check also for successful executor values
-    expect(
-      executorSettlements.map(
-        (settlement) => settlement.status === "fulfilled" && settlement.value
-      )
-    ).toEqual([0, 1, 2, 3]);
-  });
+      // CHECK RESULTS FROM OPERATIONS AND EXECUTORS
 
-  test("executor with exhausted retries fails", async () => {
-    // TEST SETUP
-    const FAILURES = 2;
-
-    const { createExecutor } = createExecutorStrategy({ retries: 2 });
-
-    const createFailingOperation = (failures: number) => {
-      let attempt = 0;
-      return async (n: number) => {
-        attempt++;
-        if (attempt <= failures) {
-          throw new Error("Job failed");
-        }
-        return n;
-      };
-    };
-
-    // CREATE OPERATIONS AND EXECUTORS
-    const operations = Array.from({ length: 4 }, () =>
-      createFailingOperation(FAILURES)
-    );
-    const executors = operations.map((operation) => createExecutor(operation));
-
-    // EXECUTE OPERATIONS AND EXECUTORS
-    const operationSettlements = await Promise.allSettled(
-      operations.map((operation, index) => operation(index))
-    );
-    const executorSettlements = await Promise.allSettled(
-      executors.map((executor, index) => executor(index))
-    );
-
-    // CHECK RESULTS FROM OPERATIONS AND EXECUTORS
-
-    // all operations will be run once
-    // and will fail
-    expect(
-      operationSettlements.every(
-        (settlement) => settlement.status === "rejected"
-      )
-    );
-
-    // all executors will be run once (but underlying operation run twice)
-    // and will therefore succeed
-    expect(
-      executorSettlements.every(
-        (settlement) => settlement.status === "rejected"
-      )
-    );
-  });
-
-  test("executor with timeout throws error for slow operations", async () => {
-    // define a slow operation and wrap in executor with timeout
-    async function slowOperation() {
-      await sleep(20);
-    }
-    const { createExecutor } = createExecutorStrategy({ timeoutMs: 10 });
-    const impatientExecutor = createExecutor(slowOperation);
-
-    {
-      // original operations
-      const start = Date.now();
-      const operationSettlements = await Promise.allSettled([
-        slowOperation(),
-        slowOperation(),
-        slowOperation(),
-        slowOperation(),
-      ]);
-      expect(Date.now() - start).toBeGreaterThanOrEqual(15);
-
+      // all operations will be run once
+      // and will fail
       expect(
         operationSettlements.every(
+          (settlement) => settlement.status === "rejected"
+        )
+      );
+
+      // all executors will be run once (but underlying operation run twice)
+      // and will therefore succeed
+      expect(
+        executorSettlements.every(
           (settlement) => settlement.status === "fulfilled"
         )
       );
-    }
 
-    {
-      // original operations
-      const start = Date.now();
-      const executorSettlements = await Promise.allSettled([
-        impatientExecutor(),
-        impatientExecutor(),
-        impatientExecutor(),
-        impatientExecutor(),
-      ]);
-      expect(Date.now() - start).toBeLessThanOrEqual(15);
+      // check also for successful executor values
+      expect(
+        executorSettlements.map(
+          (settlement) => settlement.status === "fulfilled" && settlement.value
+        )
+      ).toEqual([0, 1, 2, 3]);
+    });
 
+    test("executor with exhausted retries fails", async () => {
+      // TEST SETUP
+      const FAILURES = 2;
+
+      const { createExecutor } = createExecutorStrategy({ retries: 2 });
+
+      // CREATE OPERATIONS AND EXECUTORS
+      const operations = [
+        createFailingOperation(FAILURES),
+        createFailingOperation(FAILURES),
+        createFailingOperation(FAILURES),
+        createFailingOperation(FAILURES),
+      ];
+
+      const executors = [
+        createExecutor(operations[0]),
+        createExecutor(operations[1]),
+        createExecutor(operations[2]),
+        createExecutor(operations[3]),
+      ];
+
+      // EXECUTE OPERATIONS AND EXECUTORS
+      const operationSettlements = await Promise.allSettled(
+        operations.map((operation, index) => operation(index))
+      );
+
+      const executorSettlements = await Promise.allSettled(
+        executors.map((executor, index) => executor(index))
+      );
+
+      // CHECK RESULTS FROM OPERATIONS AND EXECUTORS
+
+      // all operations will be run once
+      // and will fail
+      expect(
+        operationSettlements.every(
+          (settlement) => settlement.status === "rejected"
+        )
+      );
+
+      // all executors will be run once (but underlying operation run twice)
+      // and will therefore succeed
       expect(
         executorSettlements.every(
           (settlement) => settlement.status === "rejected"
         )
       );
+    });
+  });
+
+  describe("Timeout", () => {
+    async function slowOperation() {
+      await sleep(20);
+      return "foo";
     }
+
+    test("executor with short timeout rejects slow operations", async () => {
+      const { createExecutor } = createExecutorStrategy({ timeoutMs: 10 });
+      const impatientExecutor = createExecutor(slowOperation);
+
+      {
+        // original operations are slow
+        const start = Date.now();
+        const operationSettlements = await Promise.allSettled([
+          slowOperation(),
+          slowOperation(),
+          slowOperation(),
+          slowOperation(),
+        ]);
+        expect(Date.now() - start).toBeGreaterThanOrEqual(15);
+
+        expect(
+          operationSettlements.every(
+            (settlement) => settlement.status === "fulfilled"
+          )
+        );
+      }
+
+      {
+        // executor operations are rejected
+        const start = Date.now();
+        const executorSettlements = await Promise.allSettled([
+          impatientExecutor(),
+          impatientExecutor(),
+          impatientExecutor(),
+          impatientExecutor(),
+        ]);
+        expect(Date.now() - start).toBeLessThanOrEqual(15);
+
+        expect(
+          executorSettlements.every(
+            (settlement) => settlement.status === "rejected"
+          )
+        );
+      }
+    });
+
+    test("executor with long timeout resolves slow operations", async () => {
+      const { createExecutor } = createExecutorStrategy({ timeoutMs: 30 });
+
+      const patientExecutor = createExecutor(slowOperation);
+
+      // original operations
+      const start = Date.now();
+      const executorSettlements = await Promise.allSettled([
+        patientExecutor(),
+        patientExecutor(),
+        patientExecutor(),
+        patientExecutor(),
+      ]);
+      expect(Date.now() - start).toBeGreaterThanOrEqual(20);
+
+      expect(
+        executorSettlements.map(
+          (settlement) => settlement.status === "fulfilled" && settlement.value
+        )
+      ).toEqual(["foo", "foo", "foo", "foo"]);
+    });
   });
 });
