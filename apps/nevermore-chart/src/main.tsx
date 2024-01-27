8118ad9d9a367cbae8f1cf4f9dd9ba2eca6d132a
Cefn Hoile <github.com@cefn.com>
2024-01-27 22:08:40 +0000

    Scaffolded Vite App

---
+++ apps/nevermore-chart/src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+function getDomElement(cssSelector: string) {
+  const el = document.querySelector(`${cssSelector}`);
+  if (el === null) {
+    throw new Error(`No element matching '${cssSelector}'`);
+  }
+  return el;
+}
+
+ReactDOM.createRoot(getDomElement("#root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
