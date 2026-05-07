723f95a30c6654cc4f2020cc36651df3781ece54
Cefn Hoile <github.com@cefn.com>
2026-05-07 14:57:58 +0100

    Increment packages with ncu. Upgrade to newest node. Delete package-lock. Update eslint config

--- eslint.config-ts.mjs
+++ eslint.config-ts.mjs
@@ -1,70 +1,17 @@
-// @ts-check
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  resolvePluginsRelativeTo: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default [
-  ...compat.extends(
-    "plugin:react/recommended",
-    "standard-with-typescript",
-    "prettier"
-  ),
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
   {
-    files: ["src/**/*.ts", "src/**/*.tsx"],
     settings: {
       react: {
         version: "detect",
       },
     },
-    rules: {
-      "@typescript-eslint/semi": [2, "always"],
-      "@typescript-eslint/non-nullable-type-assertion-style": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/no-empty-interface": [
-        "error",
-        {
-          allowSingleExtends: true,
-        },
-      ],
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          varsIgnorePattern: "^_",
-          argsIgnorePattern: "^_",
-        },
-      ],
-      "@typescript-eslint/no-invalid-void-type": [
-        "error",
-        {
-          allowAsThisParameter: true,
-        },
-      ],
-    },
-  },
-  {
-    ignores: [
-      "eslint.config.mjs",
-      "vite.config.ts",
-      "vite.config.ts*",
-      "test/examples/*.js",
-      "**/*.test.ts",
-      "dist",
-      "coverage",
-      ".wireit",
-    ],
-  },
-];
+  }
+);
